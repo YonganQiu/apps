cd5c10097b9540a07d4c94e98cc1c47b2a37cb37
zhongheng.zheng <zhongheng.zheng@5c5aebc2-7139-48a2-a469-8d061efd679e>
2012-07-16 09:16:24 +0000

    推压动画
    git-svn-id: svn://10.128.161.92/Android-ICS/branches/ua/apps@2681 5c5aebc2-7139-48a2-a469-8d061efd679e

---
+++ Launcher2/src/com/android/launcher2/ScreenScrollAnimationPush.java
@@ -0,0 +1,65 @@
+package com.android.launcher2;
+
+import android.view.View;
+
+/**
+ * added by zhongheng.zheng 2012.7.16 for Push screen scroll animation
+ * 
+ */
+
+import com.android.launcher2.Workspace.ScreenScrollAnimation;
+
+public class ScreenScrollAnimationPush implements ScreenScrollAnimation{
+	private static float TRANSITION_MAX_MULTIPLE = 0.7f;
+
+	@Override
+	public void screenScroll(float scrollProgress, View v) {
+		int pageWidth = v.getMeasuredWidth();
+		int pageHeight = v.getMeasuredHeight();
+//		CellLayout cl = (CellLayout) v;
+//		int pageWidth = cl.getChildrenLayout().getMeasuredWidth();
+//		int pageHeight = cl.getChildrenLayout().getMeasuredHeight();
+		if (Math.abs(scrollProgress) == 1.0f) {
+			v.setPivotX(pageWidth / 2.0f);
+			v.setPivotY(pageHeight / 2.0f);
+			v.setScaleX(1);
+		} else {
+			if (scrollProgress > 0) {
+				// left screen.
+				v.setPivotX(pageWidth);
+				v.setScaleX(1-scrollProgress);
+			} else {
+				// right screen.
+				v.setPivotX(0);
+				v.setScaleX(1+scrollProgress);
+			}
+		}
+
+	}
+
+	@Override
+	public void leftScreenOverScroll(float scrollProgress, View v) {
+		int pageWidth = v.getMeasuredWidth();
+		int pageHeight = v.getMeasuredHeight();
+		v.setPivotX(pageWidth);
+		v.setScaleX(1+scrollProgress*TRANSITION_MAX_MULTIPLE);
+	}
+
+	@Override
+	public void rightScreenOverScroll(float scrollProgress, View v) {
+		int pageWidth = v.getMeasuredWidth();
+		int pageHeight = v.getMeasuredHeight();
+		v.setPivotX(0);
+		v.setScaleX(1-scrollProgress*TRANSITION_MAX_MULTIPLE);
+	}
+
+	@Override
+	public void resetAnimationData(View v) {
+		int pageWidth = v.getMeasuredWidth();
+		int pageHeight = v.getMeasuredHeight();
+		v.setPivotX((pageWidth / 2.0f));
+		v.setPivotY(pageHeight / 2.0f);
+		v.setRotation(0);
+		v.setTranslationX(0);
+	}
+}
